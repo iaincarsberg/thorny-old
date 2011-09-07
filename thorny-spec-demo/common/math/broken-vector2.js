b9f5bf9d4fc8a96181ff54d41d63539dc707660d
Iain Carsberg <horrain@gmail.com>
2011-09-07 20:28:04 +0100

    Implemented some tests as apart of the override config

---
+++ thorny-spec-demo/common/math/broken-vector2.js
@@ -0,0 +1,11 @@
+/*global window*/
+(function (module) {
+	/**
+	 * Used to make an object observable
+	 * @param object subject Contains the object that is to be observed
+	 * @return object Containing the injected observable functionality.
+	 */
+	module.exports = function ($, module) {
+		return 'a broken vector2 implementation';
+	};
+}((typeof window === 'undefined') ? module : window.thorny_path('./thorny-spec-demo/common/math/broken-vector2')));
